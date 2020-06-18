1584a1df446c750d51ba8be8734206a630d42d0a
struesdale647 <44466959+struesdale647@users.noreply.github.com>
2020-06-18 19:00:13 +0100

    Final Commit.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function setGradient () {
 	body.style.background = gradientType + color1.value + " , " + color2.value + ")";
 	css.textContent = body.style.background + ";";
 };
-
+//Selects a random color in RGB format.
 function randomRGB () {
 	var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 	return randomColor
@@ -29,39 +29,38 @@ setGradient(color1.value, color2.value);
 
 //Event Listeners Below here
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
-
+//Random left color
 rand1.addEventListener("click", function(){
 	color1.value = randomRGB();
 	setGradient();
 });
-
+//Random right color
 rand2.addEventListener("click", function(){
 	color2.value = randomRGB();
 	setGradient();
 });
-
+//Random colors for both
 randBoth.addEventListener("click", function(){
 	color1.value = randomRGB();
 	color2.value = randomRGB();
 	setGradient();
 });
-
+//Swap color pallettes
 swap.addEventListener("click", function(){
 	var temp = color1.value;
 	color1.value = color2.value;
 	color2.value = temp;
 	setGradient();
 });
-
+//Horizontal Radio Button
 radio[0].addEventListener("click", function(){
 	rand1.textContent = "Random Left";
 	rand2.textContent = "Random Right";
 	gradientType = "linear-gradient(to right, ";
 	setGradient();
 });
-
+//Vertical Radio Button
 radio[1].addEventListener("click", function(){
 	rand1.textContent = "Random Top";
 	rand2.textContent = "Random Bottom";
@@ -69,23 +68,23 @@ radio[1].addEventListener("click", function(){
 	setGradient();
 });
 
-radio[2].addEventListener("click", function(){
-	rand1.textContent = "Random Centre";
-	rand2.textContent = "Random Edge";
-	gradientType = "radial-gradient(";
-	setGradient();
-});
+// radio[2].addEventListener("click", function(){
+// 	rand1.textContent = "Random Centre";
+// 	rand2.textContent = "Random Edge";
+// 	gradientType = "radial-gradient(";
+// 	setGradient();
+// });
 
-radio[3].addEventListener("click", function(){
-	rand1.textContent = "Random Top";
-	rand2.textContent = "Random Bottom";
-	gradientType = "linear-gradient(to right bottom, ";
-	setGradient();
-});
+// radio[3].addEventListener("click", function(){
+// 	rand1.textContent = "Random Top";
+// 	rand2.textContent = "Random Bottom";
+// 	gradientType = "linear-gradient(to right bottom, ";
+// 	setGradient();
+// });
 
-radio[4].addEventListener("click", function(){
-	rand1.textContent = "Random Top";
-	rand2.textContent = "Random Bottom";
-	gradientType = "linear-gradient(to left bottom, ";
-	setGradient();
-});
+// radio[4].addEventListener("click", function(){
+// 	rand1.textContent = "Random Top";
+// 	rand2.textContent = "Random Bottom";
+// 	gradientType = "linear-gradient(to left bottom, ";
+// 	setGradient();
+// });
